4adaa8275c744c57d0639d31c7533b45954b092f
ryunosukesatou <96858418+ryunosukesatou@users.noreply.github.com>
2022-05-16 05:14:46 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -18,4 +18,15 @@ $(function(){
         pauseOnDotsHover: true,//ドットナビゲーションをマウスホバーで一時停止を無効
 });
 
+    $('.nav-btn').click(function(){
+        $('nav ul').animate({width:'show'});
+        $('nav ul li span').show();
+        $('nav').fadeIn();
+    });
+
+    $('nav').click(function(){
+        $('nav ul').animate({width:'hide'});
+        $('nav ul li span').hide();
+        $('nav').fadeOut();
+    });
  });
